ef5014af1a4a1d961d56bc6290567fe621370158
Hemangi <hemangigalledar@gmail.com>
2026-06-02 13:21:43 +0530

    gujlish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import TextForm from './Textform';
+import { useState } from 'react';
 
-function App() {
+
+function App(props) {
+  const[dark,setDark]=useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <TextForm dark={dark}/>
+    
+    
+    </>
   );
 }
 
